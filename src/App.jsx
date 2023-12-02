c520186bfc950d60527438fb60d3e6d4872e171d
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-02 21:39:12 +0530

    lastest commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import cross_icon from '../Assets/cross.png'
 import circle_icon from '../Assets/circle.png'
 
 import './App.css'
-import Confetti from './celebration.jsx'; 
+import Confetti from './celebration.jsx';
 
 
 let data = ["", "", "", "", "", "", "", "", ""]
@@ -14,21 +14,20 @@ function App() {
   let [count, setCount] = useState(0)
   let [winStatus, setWinStatus] = useState(false);
 
-  let [currentPlayer, setCurrentPlayer] = useState('x'); 
+  let [currentPlayer, setCurrentPlayer] = useState('x');
 
   let [lock, setLock] = useState(false)
   let titleRef = useRef(null)
   let box1 = useRef(null)
   let box2 = useRef(null)
-  let box3= useRef(null)
-  let box4 =useRef(null)
-  let box5 =useRef(null)
-  let box6= useRef(null)
-  let box7= useRef(null)
-  let box8= useRef(null)
-  let box9=useRef(null)
-
-  let box_array =[box1,box2,box3,box4,box5,box6,box7,box8,box9]
+  let box3 = useRef(null)
+  let box4 = useRef(null)
+  let box5 = useRef(null)
+  let box6 = useRef(null)
+  let box7 = useRef(null)
+  let box8 = useRef(null)
+  let box9 = useRef(null)
+  let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 
   const toggle = (e, num) => {
     if (lock) {
@@ -41,7 +40,7 @@ function App() {
       setCount(++count);
 
     } else {
-      e.target.innerHTML = `<img src='${circle_icon}'>` ;
+      e.target.innerHTML = `<img src='${circle_icon}'>`;
       data[num] = "o";
       setCurrentPlayer('x')
       setCount(++count);
@@ -75,25 +74,22 @@ function App() {
       won(data[8]);
     } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
       console.log("conmes 7")
-
       won(data[8]);
     } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
       console.log("conmes 8")
-
       won(data[2]);
     } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
       console.log("conmes 9")
-
       won(data[6]);
     }
-    else{
+    else {
       let draw = true;
       data.forEach(cell => {
         if (cell === "") {
           draw = false;
         }
       });
-    
+
       if (draw) {
         won('draw');
       }
@@ -102,13 +98,13 @@ function App() {
 
   const won = (winner) => {
     setLock(true);
-    if(winner==='x'){
-      console.log('titleRef -->',titleRef);
-      titleRef.current.innerHTML=`congratulation: <img src='${cross_icon}'> Won`
-    }else if(winner =='o'){
-      titleRef.current.innerHTML=`congratulation: <img src='${circle_icon}'> Won`
-    }else{
-       titleRef.current.innerHTML=`Its a Draw`
+    if (winner === 'x') {
+      console.log('titleRef -->', titleRef);
+      titleRef.current.innerHTML = `congratulation: <img src='${cross_icon}'> Won`
+    } else if (winner == 'o') {
+      titleRef.current.innerHTML = `congratulation: <img src='${circle_icon}'> Won`
+    } else {
+      titleRef.current.innerHTML = `Its a Draw`
     }
 
     if (winner === 'x' || winner === 'o') {
@@ -118,13 +114,13 @@ function App() {
     }
   }
 
-  const reset=()=>{
+  const reset = () => {
     setLock(false)
     setWinStatus(false)
     data = ["", "", "", "", "", "", "", "", ""]
-    titleRef.current.innerHTML='Tic Tac Toe ';
-    box_array.map((e)=>{
-      e.current.innerHTML="";
+    titleRef.current.innerHTML = 'Tic Tac Toe ';
+    box_array.map((e) => {
+      e.current.innerHTML = "";
     })
   }
   return (
